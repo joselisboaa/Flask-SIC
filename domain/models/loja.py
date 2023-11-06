317882aba4ebee9641cf12f4b20f4d1ba09523fa
joselisboaa <joselisboaa05@gmail.com>
2023-11-06 17:43:04 -0300

    fix: corrigindo o nome do campo para o português

--- domain/models/loja.py
+++ domain/models/loja.py
@@ -4,6 +4,6 @@ from db import db
 class LojaModel(db.Model):
     __tablename__ = "lojas"
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(255), nullable=False, unique=True)
+    nome = db.Column(db.String(255), nullable=False, unique=True)
 
     clientes = db.relationship("ClienteModel", back_populates="loja", lazy="dynamic")

dc16e90a7183b02594022d94acf0045bf7a41ff5
joselisboaa <joselisboaa05@gmail.com>
2023-11-06 20:35:48 -0300

    feat: método para criar lojas

--- domain/services/loja.py
+++ domain/services/loja.py
@@ -1,9 +1,31 @@
-from db import db
+from flask import make_response, jsonify
 from domain.models import LojaModel
+from api.schemas import PlainLojaSchema
+
+from db import db
 
 
 class LojaService:
-
+    # método responsável por pegar todos as Lojas que estão registradas no banco de dados
     def get_all(self):
+        lojas = LojaModel.query.all()
 
-        return LojaModel.query.all()
+        return make_response(jsonify(lojas))
+
+    # método responsável por criar um registro de Loja no banco de dados
+    def create(self, loja_data):
+        loja = LojaModel(**loja_data)
+
+        self.save_loja(loja)
+
+        return make_response(jsonify(
+            {
+                "message": "Loja criada com sucesso!",
+                "loja": PlainLojaSchema().dump(loja)
+            }
+        ), 201)
+
+    @staticmethod
+    def save_loja(loja):
+        db.session.add(loja)
+        db.session.commit()

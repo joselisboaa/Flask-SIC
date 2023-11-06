de6d3ded6a1024fba15c4c98c948f02fcf121cec
joselisboaa <joselisboaa05@gmail.com>
2023-11-05 23:10:15 -0300

    feat: criação do modelo de relacional de cliente e da relação cliente-loja

---
+++ domain/models/cliente.py
@@ -0,0 +1,15 @@
+from db import db
+
+
+# Classe do modelo da entidade relacional
+class ClienteModel(db.Model):
+    __tablename__ = "clintes"
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=False)
+    endereco = db.Column(db.String(255), nullable=False, unique=False)
+
+    # Lado muitos (many) guarda a chave estrangeira
+    loja_id = db.Column(db.Integer(), db.ForeignKey("lojas.id"), nullable=False)
+
+    # Uma loja tem vários clientes
+    loja = db.relationship("LojaModel", back_populates="clientes", uselist=False)

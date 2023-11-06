dc7b8d37c47981c6af9d1be29fa740528247e277
joselisboaa <joselisboaa05@gmail.com>
2023-11-06 17:42:40 -0300

    fix: corrigindo nome da tabela

--- domain/models/cliente.py
+++ domain/models/cliente.py
@@ -3,7 +3,7 @@ from db import db
 
 # Classe do modelo da entidade relacional
 class ClienteModel(db.Model):
-    __tablename__ = "clintes"
+    __tablename__ = "clientes"
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=False)
     endereco = db.Column(db.String(255), nullable=False, unique=False)

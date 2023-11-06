c47b780057073b8254598d04509b6430f75fa8b2
joselisboaa <joselisboaa05@gmail.com>
2023-11-05 23:09:25 -0300

    feat: modulo de instanciação do flask sqlalchemy

---
+++ db.py
@@ -0,0 +1,9 @@
+"""
+Nesse módulo é feito parte do gerenciamento do banco de dados
+
+o Flask_SQLAlchemy provê uma interface para trabalharmos com banco de dados com Flask
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()

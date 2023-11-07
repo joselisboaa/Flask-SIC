7156b82212696d1130b36a66d55c497ec02d030c
joselisboaa <joselisboaa05@gmail.com>
2023-11-06 21:31:20 -0300

    feat: adição do formatador de consultas com listas

---
+++ utils/formatters/query.py
@@ -0,0 +1,14 @@
+
+
+class QueryFormatter:
+    # Esse método servirá para podermos transformar os dados numa
+    # Lista de Schema para podermos fazer o jsonify
+    def query_list_to_schema_list(self, query_result: list, schema):
+        schematized_data_list = []
+
+        for item in query_result:
+            schematized_data = schema().dump(item)
+
+            schematized_data_list.append(schematized_data)
+
+        return schematized_data_list

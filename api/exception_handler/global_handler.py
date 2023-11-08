bbab0e92c06ad6b652bd92348b451e2bd7f55a21
joselisboaa <joselisboaa05@gmail.com>
2023-11-08 00:13:39 -0300

    feat: adição de tratamento de restrição de campo único

--- api/exception_handler/global_handler.py
+++ api/exception_handler/global_handler.py
@@ -3,7 +3,8 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from utils.exceptions import (
     NotFoundException,
-    DependencyEntityNotExist
+    DependencyEntityNotExist,
+    UniqueException
 )
 
 
@@ -17,8 +18,17 @@ def global_error_handling(app):
             details=error.details,
         ).to_json()
 
+    @app.errorhandler(UniqueException)
+    def unique_field_constraint_exception(error):
+        return UniqueException(
+            message=error.message,
+            http_method=error.http_method,
+            status_code=error.status_code,
+            details=error.details,
+        ).to_json()
+
     @app.errorhandler(DependencyEntityNotExist)
-    def not_found_constraint_error(error):
+    def dependency_entity_not_exist_constraint_error(error):
         return DependencyEntityNotExist(
             message=error.message,
             http_method=error.http_method,

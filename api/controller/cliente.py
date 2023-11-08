0ad2a9d84917dfba4285bff5a3867e50fecb9484
joselisboaa <joselisboaa05@gmail.com>
2023-11-08 00:01:13 -0300

    fix: chamada de métodos do service no controller

--- api/controller/cliente.py
+++ api/controller/cliente.py
@@ -2,21 +2,23 @@ from flask.views import MethodView
 from flask_smorest import Blueprint
 
 from api.schemas import ClienteSchema, ClientQuerySchemas
+from domain.services import ClienteService
 
 blp = Blueprint("Clientes", __name__, "Operações de clientes")
+
 
 @blp.route('/v1/clientes')
 class Clientes(MethodView):
 
-    @blp.arguments(ClientQuerySchemas, location='query')
+    @blp.arguments(ClientQuerySchemas)
     @blp.response(200, ClienteSchema(many=True))
-    def get(self, *args):
-        pass
+    def get(self):
+        return ClienteService().get_all()
 
     @blp.arguments(ClienteSchema)
     @blp.response(201, ClienteSchema)
     def post(self, cliente_data):
-        pass
+        return ClienteService().create(cliente_data)
 
 
 @blp.route('/v1/clientes/<int:cliente_id>')
@@ -25,13 +27,18 @@ class Cliente(MethodView):
     @blp.arguments(ClienteSchema)
     @blp.response(200, ClienteSchema)
     def get(self, cliente_id):
-        pass
+        return ClienteService().get_by_id(cliente_id)
 
     @blp.response(204)
     def delete(self, cliente_id):
-        pass
+        return ClienteService().delete_by_id(cliente_id)
+
+    @blp.arguments(ClienteSchema)
+    @blp.response(200, ClienteSchema)
+    def patch(self, cliente_data, cliente_id):
+        return ClienteService().patch(cliente_data, cliente_id)
 
     @blp.arguments(ClienteSchema)
     @blp.response(200, ClienteSchema)
     def put(self, cliente_data, cliente_id):
-        pass
+        return ClienteService().update(cliente_data, cliente_id)

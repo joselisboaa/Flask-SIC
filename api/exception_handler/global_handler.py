b1b9c6099c9b291dec2120f304c650e0e4ed2d78
joselisboaa <joselisboaa05@gmail.com>
2023-11-08 00:03:53 -0300

    feat: registro da exceção de entidade dependente não existente

--- api/exception_handler/global_handler.py
+++ api/exception_handler/global_handler.py
@@ -2,7 +2,8 @@ from flask import make_response, jsonify
 from sqlalchemy.exc import SQLAlchemyError
 
 from utils.exceptions import (
-    NotFoundException
+    NotFoundException,
+    DependencyEntityNotExist
 )
 
 
@@ -10,6 +11,15 @@ def global_error_handling(app):
     @app.errorhandler(NotFoundException)
     def not_found_constraint_error(error):
         return NotFoundException(
+            message=error.message,
+            http_method=error.http_method,
+            status_code=error.status_code,
+            details=error.details,
+        ).to_json()
+
+    @app.errorhandler(DependencyEntityNotExist)
+    def not_found_constraint_error(error):
+        return DependencyEntityNotExist(
             message=error.message,
             http_method=error.http_method,
             status_code=error.status_code,
